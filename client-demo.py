402ddc0eb2b929801950398cd57a62c9452ea607
Shaoming Feng <frank@houzz.com>
2016-07-19 23:49:38 -0700

    Add a higher level API "client"
    Hide parameter details and use just like the app.

---
+++ client-demo.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2016 superfsm@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import os
+import re
+import json
+import struct
+import logging
+import requests
+import argparse
+import getpass
+
+from client import Client
+
+from pgoapi import PGoApi
+from pgoapi.utilities import f2i, h2f
+
+from google.protobuf.internal import encoder
+from geopy.geocoders import GoogleV3
+from s2sphere import CellId, LatLng
+
+log = logging.getLogger(__name__)
+
+def get_pos_by_name(location_name):
+    geolocator = GoogleV3()
+    loc = geolocator.geocode(location_name)
+
+    log.info('Your given location: %s', loc.address.encode('utf-8'))
+    log.info('lat/long/alt: %s %s %s', loc.latitude, loc.longitude, loc.altitude)
+
+    return (loc.latitude, loc.longitude)
+
+def init_config():
+    parser = argparse.ArgumentParser()
+    config_file = "config.json"
+
+    # If config file exists, load variables from json
+    load   = {}
+    if os.path.isfile(config_file):
+        with open(config_file) as data:
+            load.update(json.load(data))
+
+    # Read passed in Arguments
+    required = lambda x: not x in load
+    parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
+        required=required("auth_service"))
+    parser.add_argument("-u", "--username", help="Username", required=required("username"))
+    parser.add_argument("-p", "--password", help="Password")
+    parser.add_argument("-l", "--location", help="Location", required=required("location"))
+    parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
+    parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
+    parser.set_defaults(DEBUG=False, TEST=False)
+    config = parser.parse_args()
+
+    # Passed in arguments shoud trump
+    for key in config.__dict__:
+        if key in load and config.__dict__[key] == None:
+            config.__dict__[key] = load[key]
+
+    # Get password fron stdin if no exist
+    if config.__dict__['password'] is None:
+        config.__dict__['password'] = getpass.getpass('Password:')
+
+    if config.auth_service not in ['ptc', 'google']:
+      log.error("Invalid Auth service specified! ('ptc' or 'google')")
+      return None
+
+    return config
+
+
+def main():
+    # log settings
+    # log format
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+    # log level for http request class
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    # log level for main pgoapi class
+    logging.getLogger("pgoapi").setLevel(logging.INFO)
+    # log level for internal pgoapi class
+    logging.getLogger("rpc_api").setLevel(logging.INFO)
+
+    config = init_config()
+    if not config:
+        return
+
+    if config.debug:
+        logging.getLogger("client").setLevel(logging.DEBUG)
+        logging.getLogger("requests").setLevel(logging.DEBUG)
+        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
+        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
+
+    # provide player position on the earth
+    position = get_pos_by_name(config.location)
+    if config.test:
+        return
+
+    # instantiate client:
+    client = Client()
+
+    # login
+    if not client.login(config.auth_service, config.username, config.password):
+        return
+
+    # Operate on client
+    # client.move_to(*position)
+    # client.scan()
+    # client.get_player()
+    # client.get_inventory()
+    # client.get_map_objects()
+
+    ################################################ Test code
+    client.jump_to(*position)
+    client.scan()
+    while True:
+        i = client.pokestop[0]
+        print i['id'], i['enabled'], i['dist']
+        client.move_to_obj(i)
+        client.spin(i)
+        client.scan
+        break
+
+    ################################################ Need to move to client.py
+
+    # spin a fort
+    # ----------------------
+    #fortid = '<your fortid>'
+    #lng = <your longitude>
+    #lat = <your latitude>
+    #api.fort_search(fort_id=fortid, fort_latitude=lat, fort_longitude=lng, player_latitude=f2i(position[0]), player_longitude=f2i(position[1]))
+
+    # release/transfer a pokemon and get candy for it
+    # ----------------------
+    #api.release_pokemon(pokemon_id = <your pokemonid>)
+
+    # get download settings call
+    # ----------------------
+    #api.download_settings(hash="4a2e9bc330dae60e7b74fc85b98868ab4700802e")
+
+    # execute the RPC call
+    # response_dict = api.call()
+    # print('Response dictionary: \n\r{}'.format(json.dumps(response_dict, indent=2)))
+
+    # alternative:
+    # api.get_player().get_inventory().get_map_objects().download_settings(hash="4a2e9bc330dae60e7b74fc85b98868ab4700802e").call()
+
+if __name__ == '__main__':
+    main()
---
+++ client.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2016 superfsm@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import os
+import re
+import json
+import struct
+import logging
+import requests
+import argparse
+import getpass
+import time
+
+from pgoapi import PGoApi
+from pgoapi.utilities import f2i, h2f
+
+from google.protobuf.internal import encoder
+from geopy.geocoders import GoogleV3
+from geopy.distance import great_circle
+from s2sphere import CellId, LatLng
+
+log = logging.getLogger(__name__)
+
+class Client:
+
+    def __init__(self):
+        self._api = PGoApi()
+
+        self._lat = 0
+        self._lng = 0
+        self._alt = 0
+
+        self._lat_f2i = 0
+        self._lng_f2i = 0
+        self._alt_f2i = 0
+
+        self.profile = {}
+        self.item = {}
+        self.pokemon = {}
+
+        self.pokestop = []
+        self.wild = []
+
+    def get_position(self):
+        return (self._lat, self._lng)
+
+    # Move to object with at most 50m/s
+    def move_to_obj(self, obj):
+        self.move_to(obj['lat'], obj['lng'])
+
+    # Move to position with at most 50m/s
+    def move_to(self, lat, lng):
+        a = (self._lat, self._lng)
+        b = (lat, lng)
+
+        dist = great_circle(a, b).meters
+        steps = int(dist / 50) + 1
+
+        delta_lat = (lat - self._lat) / steps
+        delta_lng = (lng - self._lng) / steps
+
+        for step in range(steps):
+            self.jump_to(self._lat + delta_lat, self._lng + delta_lng)
+            time.sleep(1)
+
+    # Jump to position
+    def jump_to(self, lat, lng, alt = 0):
+        log.debug('Move to - Lat: %s Long: %s Alt: %s', lat, lng, alt)
+
+        self._api.set_position(lat, lng, 0)
+
+        self._lat = lat
+        self._lng = lng
+        # self._alt = alt
+
+        self._lat_f2i = f2i(lat)
+        self._lng_f2i = f2i(lng)
+        # self._alt_f2i = f2i(alt)
+
+        self._sort_map()
+
+    # Distance to an object
+    def _dist_to_obj(self, obj):
+        a = (self._lat, self._lng)
+        b = (obj['lat'], obj['lng'])
+        return great_circle(a, b).meters
+
+    # Sort the items on map by distance
+    def _sort_map(self):
+        for i in self.pokestop:
+            i['dist'] = self._dist_to_obj(i)
+        self.pokestop = sorted(self.pokestop , key=lambda k: k['dist'])
+        for i in self.wild:
+            i['dist'] = self._dist_to_obj(i)
+        self.wild = sorted(self.pokestop , key=lambda k: k['dist'])
+
+    # Scan the map around you
+    def scan(self):
+
+        self.pokestop = []
+        self.wild = []
+
+        timestamp = "\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000"
+        cellid = self._get_cellid()
+        self._api.get_map_objects(
+            latitude=self._lat_f2i,
+            longitude=self._lng_f2i,
+            since_timestamp_ms=timestamp,
+            cell_id=cellid)
+        resp = self._api.call()
+        for map_cell in resp['responses']['GET_MAP_OBJECTS']['map_cells']:
+            if 'forts' in map_cell:
+                for fort in map_cell['forts']:
+                    if 'type' in fort and fort['type'] == 1:
+                        pokestop = {}
+                        pokestop['id'] = fort['id']
+                        pokestop['lat'] = fort['latitude']
+                        pokestop['lng'] = fort['longitude']
+                        pokestop['enabled'] = fort['enabled']
+                        self.pokestop.append(pokestop)
+            if 'wild_pokemons' in map_cell:
+                for wild_pokemon in map_cell['wild_pokemons']:
+                    wild = {}
+                    wild['encounter_id'] = wild_pokemon['encounter_id']
+                    wild['spawnpoint_id'] = wild_pokemon['spawnpoint_id']
+                    wild['encounter_id'] = wild_pokemon['encounter_id']
+                    wild['lat'] = wild_pokemon['latitude']
+                    wild['lng'] = wild_pokemon['longitude']
+                    wild['spawnpoint_id'] = wild_pokemon['spawnpoint_id']
+                    wild['last_modified_timestamp_ms'] = wild_pokemon['last_modified_timestamp_ms']
+                    wild['time_till_hidden_ms'] = wild_pokemon['time_till_hidden_ms']
+                    wild['expire_ms'] = time.time() * 1000 + wild_pokemon['time_till_hidden_ms']
+                    self.wild.append(wild)
+
+        self._sort_map()
+        log.debug('Response dictionary: \n\r{}'.format(json.dumps(resp, indent=2)))
+
+    def spin(self, pokestop):
+        self._api.fort_search(fort_id=pokestop['id'],
+            fort_latitude=pokestop['lat'],
+            fort_longitude=pokestop['lng'],
+            player_latitude=self._lat_f2i ,
+            player_longitude=self._lng_f2i)
+
+        resp = self._api.call()
+        log.info('Response dictionary: \n\r{}'.format(json.dumps(resp, indent=2)))
+
+    def login(self, auth_service, username, password):
+        return self._api.login(auth_service, username, password)
+
+    def get_player(self):
+        self._api.get_player()
+        resp = self._api.call()
+        self.profile['username'] = resp['responses']['GET_PLAYER']['profile']['username']
+
+        log.debug('Response dictionary: \n\r{}'.format(json.dumps(resp, indent=2)))
+
+    def get_inventory(self):
+        self._api.get_inventory()
+        resp = self._api.call()
+
+        # Save items in self.item
+        for inventory_item in resp['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']:
+            print inventory_item
+
+            try:
+                if inventory_item['inventory_item_data']['item']['item'] == 1:
+                    self.item['POKEBALL'] = inventory_item['inventory_item_data']['item']['count']
+            except KeyError:
+                pass
+
+        log.debug('Response dictionary: \n\r{}'.format(json.dumps(resp, indent=2)))
+
+    def _get_cellid(self):
+        lat = self._lat
+        long = self._lng
+        origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
+        walk = [origin.id()]
+
+        # 10 before and 10 after
+        next = origin.next()
+        prev = origin.prev()
+        for i in range(10):
+            walk.append(prev.id())
+            walk.append(next.id())
+            next = next.next()
+            prev = prev.prev()
+        return ''.join(map(self._encode, sorted(walk)))
+
+    def _encode(self,cellid):
+        output = []
+        encoder._VarintEncoder()(output.append, cellid)
+        return ''.join(output)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
