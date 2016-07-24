0537277c2f09d48bf1834b7d76a82efe12eda2ee
Shaoming Feng <frank@houzz.com>
2016-07-24 11:59:34 -0700

    do not show map all the times

--- bot.py
+++ bot.py
@@ -215,11 +215,12 @@ def main():
 
 
     client.scan().summary().status()
+    sorted_pokestops = TSP(client.get_pokestop()).solve()
+    show_map(sorted_pokestops, [])
 
     while True:
         time.sleep(5)
         sorted_pokestops = TSP(client.get_pokestop()).solve()
-        show_map(sorted_pokestops, [])
         for pokestop in sorted_pokestops:
             for wild_pokemon in client.get_wild_pokemon():
                 client.move_to_obj(wild_pokemon).catch_pokemon(wild_pokemon).scan().status()
